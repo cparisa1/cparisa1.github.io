e3d4fc51d0305dc406504280d9bd61f2ba9c79d2
Parisa <parisachandrasekhar@gmail.com>
2020-04-10 15:44:43 -0400

    Updated Page

--- myScript.js
+++ myScript.js
@@ -1,3 +1,8 @@
-function myFunction() {
- document.getElementById("demo").innerHTML = "Paragraph changed.";
+function redirect(name) {
+	if(name == 'linkedin')
+		window.open("https://www.linkedin.com/in/chandrasekhar-parisa-25a68685/","_blank");
+	else if(name == 'github')
+		window.open("https://github.com/cparisa1/","_blank");
+	else if(name=='resume')
+		window.open("files/resume.pdf","_blank");
 }
